4858175450d4e1f64b7459cb6c6319049abdae86
DIVYANAND A P <divyanand942@gmail.com>
2025-12-18 00:22:59 +0530

    Update firebase-config.js

--- noq-admin/js/firebase-config.js
+++ noq-admin/js/firebase-config.js
@@ -1,8 +1,3 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAHKV4IFAeJD6Dinxyhu0EBu8TSX_4Woqs",
-  authDomain: "noq-eats-srm-27fda.firebaseapp.com",
-  projectId: "noq-eats-srm-27fda",
-  storageBucket: "noq-eats-srm-27fda.firebasestorage.app",
-  messagingSenderId: "424387663522",
-  appId: "1:424387663522:web:94a02f022263a096b8a9fb"
+  
 };
